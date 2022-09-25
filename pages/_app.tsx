b3871c1379138aa4a80256f855e371a4f9af16d6
Danh Nguyen <danhnguyen267@gmail.com>
2022-09-24 22:50:10 -0700

    Using Next Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import '@fontsource/open-sans/700.css'
 import React from 'react'
 import Script from 'next/script'
 import { ChakraProvider } from '@chakra-ui/react'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 import theme from '../styles/theme'
 
@@ -15,20 +16,9 @@ function App({ Component, pageProps }) {
 	return (
 		<>
 			<ChakraProvider theme={theme}>
+				<GoogleAnalytics trackPageViews />
 				<Component {...pageProps} />
 			</ChakraProvider>
-			<Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-			<Script id='google-analytics' strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}>
-				{`
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){dataLayer.push(arguments);}
-					gtag('js', new Date());
-
-					gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-						page_path: window.location.pathname,
-					});
-				`}
-			</Script>
 		</>
 	)
 }
